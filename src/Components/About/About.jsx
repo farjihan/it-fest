720934f21d3072894f2861ba0ece12a8c75a8163
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2023-11-09 18:42:45 +0700

    responsive fix

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,13 +6,15 @@ export default function About() {
       <h2 className="about-title">
         Apa itu <span className="text-blue">KOMINFO?</span>
       </h2>
-      <p className="about-desc">KEMENTRIAN INDONESIA</p>
-      <p className='about-main'>
-        Kementerian Komunikasi dan Informatika adalah kementerian Indonesia yang
-        mengurusi bidang komunikasi dan informatika. Kementerian Komunikasi dan
-        Informatika sebelumnya bernama Kementerian Negara Komunikasi dan
-        Informasi dan Departemen Komunikasi dan Informatika.
-      </p>
+      <div className="about-content">
+        <p className="about-desc">KEMENTRIAN INDONESIA</p>
+        <p className='about-main'>
+          Kementerian Komunikasi dan Informatika adalah kementerian Indonesia yang
+          mengurusi bidang komunikasi dan informatika. Kementerian Komunikasi dan
+          Informatika sebelumnya bernama Kementerian Negara Komunikasi dan
+          Informasi dan Departemen Komunikasi dan Informatika.
+        </p>
+      </div>
     </section>
   );
 }
